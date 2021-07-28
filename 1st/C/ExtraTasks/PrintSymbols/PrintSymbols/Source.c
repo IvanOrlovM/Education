80d7ffd30afb9c2dac0bca2b70ed5aba1ca0ae72
Ivan <orlovivan1907@outlook.com>
2021-07-28 19:34:01 +0300

    PrintSymbols print line

--- 1st/C/ExtraTasks/PrintSymbols/PrintSymbols/Source.c
+++ 1st/C/ExtraTasks/PrintSymbols/PrintSymbols/Source.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-	printf("*");
+	int a;
+	printf("Enter count of stars: ");
+	scanf_s("%d", &a);
+	for(int i = 0; i < a; i++) 
+	{
+		printf("*");
+	}
 }
