4be1f3656d95452e6635e8468c709235371822d0
Ivan Orlov <orlovivan1907@outlook.com>
2021-08-14 13:45:39 +0300

    RandomValuesArray implemented base logic

--- 1st/C/ExtraTasks/MallocMethod/RandomValuesArray/RandomValuesArray/Source.c
+++ 1st/C/ExtraTasks/MallocMethod/RandomValuesArray/RandomValuesArray/Source.c
@@ -1,8 +1,27 @@
-#include <stdio.h>
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void main()
 {
-	int a;
-	printf("hi");
-	return 0;
+	int size;
+	printf("Enter size of array: ");
+	scanf_s("%d", &size);
+	
+	int *arr = malloc(size * sizeof(int));
+	
+	srand(time(NULL));
+	for (int i = 0; i < size; i++)
+	{
+		printf("arr[%d]=", i);
+		arr[i] = rand();
+		printf("%d\n", arr[i]);
+	}
+
+	printf("\n");
+	free(arr);
 }
+
+// реализовать пользовательский ввод 12:34 - 12:39
+// создать массив размерностью пользовательского ввода 12:40 - 12:58
+// заполнить массив случайными значениями 13:20 - 13:31
