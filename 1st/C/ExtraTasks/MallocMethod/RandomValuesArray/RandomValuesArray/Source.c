8a548a57be4bf4a173ec98d99ab1b1c0894a5ea1
Ivan Orlov <orlovivan1907@outlook.com>
2021-08-14 14:29:41 +0300

    RandomValuesArray refactoring is done

--- 1st/C/ExtraTasks/MallocMethod/RandomValuesArray/RandomValuesArray/Source.c
+++ 1st/C/ExtraTasks/MallocMethod/RandomValuesArray/RandomValuesArray/Source.c
@@ -1,27 +1,60 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "Source.h"
 
 void main()
+{
+	srand(time(NULL));
+
+	int size = UserInput();
+	
+	int *arr = CreateArray(size);
+
+	FillInArray(arr, size);
+
+	ShowArray(arr, size);
+
+	free(arr);
+}
+
+int UserInput() 
 {
 	int size;
 	printf("Enter size of array: ");
 	scanf_s("%d", &size);
-	
-	int *arr = malloc(size * sizeof(int));
-	
-	srand(time(NULL));
+	return size;
+}
+
+int* CreateArray(int size) 
+{
+	int* arr = malloc(size * sizeof(int));
+	return arr;
+}
+
+int* FillInArray(int* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand();
+	}
+
+	return arr;
+}
+
+void ShowArray(int*arr, int size) 
+{
 	for (int i = 0; i < size; i++)
 	{
 		printf("arr[%d]=", i);
-		arr[i] = rand();
 		printf("%d\n", arr[i]);
 	}
 
 	printf("\n");
-	free(arr);
 }
 
 // реализовать пользовательский ввод 12:34 - 12:39
 // создать массив размерностью пользовательского ввода 12:40 - 12:58
 // заполнить массив случайными значениями 13:20 - 13:31
+// рефакторинг 13:47
+//
---
+++ 1st/C/ExtraTasks/MallocMethod/RandomValuesArray/RandomValuesArray/Source.h
@@ -0,0 +1,5 @@
+#pragma once
+int UserInput();
+int* CreateArray(int size);
+int* FillInArray(int* arr, int size);
+void ShowArray(int* arr, int size);
