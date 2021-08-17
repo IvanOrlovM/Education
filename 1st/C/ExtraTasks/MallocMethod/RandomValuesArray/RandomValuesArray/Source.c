e6daecdbc6008daaf510e3e5d6b262bcfd0c0420
Ivan Orlov <orlovivan1907@outlook.com>
2021-08-17 18:30:39 +0300

    RandomValuesArray is done

--- 1st/C/ExtraTasks/MallocMethod/RandomValuesArray/RandomValuesArray/Source.c
+++ 1st/C/ExtraTasks/MallocMethod/RandomValuesArray/RandomValuesArray/Source.c
@@ -11,19 +11,9 @@ void main()
 	
 	int *arr = CreateArray(size);
 
-	int columnsCount = 3;
-	int rowsCount = 4;
-
-	// two - dimensional array
-	int **twoDimArr = CreateTwoDimArray(columnsCount, rowsCount);
-
 	FillInArray(arr, size);
 
-	FillInTwoDimArray(twoDimArr, columnsCount, rowsCount);
-
 	ShowArray(arr, size);
-
-	ShowTwoDimArray(twoDimArr, columnsCount, rowsCount);
 
 	free(arr);
 }
@@ -42,40 +32,11 @@ int* CreateArray(int size)
 	return arr;
 }
 
-int** CreateTwoDimArray(int columnsCount, int rowsCount)
-{
-	// указатель для блока памяти для массива указателей
-	int** table;
-
-	// выделяем память для двухмерного массива
-	table = calloc(rowsCount, sizeof(int*));
-	
-	for (int i = 0; i < rowsCount; i++)
-	{
-		table[i] = CreateArray(columnsCount);
-	}
-
-	return table;
-}
-
 int* FillInArray(int* arr, int size)
 {
 	for (int i = 0; i < size; i++)
 	{
 		arr[i] = rand();
-	}
-
-	return arr;
-}
-
-int** FillInTwoDimArray(int** arr, int columnsCount, int rowsCount)
-{
-	for (int i = 0; i < rowsCount; i++)
-	{
-		for (int j = 0; j < columnsCount; j++)
-		{
-			arr[i][j] = rand();
-		}
 	}
 
 	return arr;
@@ -90,19 +51,6 @@ void ShowArray(int*arr, int size)
 	}
 
 	printf("\n");
-}
-
-void ShowTwoDimArray(int** arr, int columnsCount, int rowsCount)
-{
-	for (int i = 0; i < rowsCount; i++)
-	{
-		for (int j = 0; j < columnsCount; j++)
-		{
-			printf("%d ", arr[i][j]);
-		}
-
-		printf("\n");
-	}
 }
 
 // реализовать пользовательский ввод 12:34 - 12:39
--- 1st/C/ExtraTasks/MallocMethod/RandomValuesArray/RandomValuesArray/Source.h
+++ 1st/C/ExtraTasks/MallocMethod/RandomValuesArray/RandomValuesArray/Source.h
@@ -1,9 +1,5 @@
 #pragma once
-void main();
 int UserInput();
 int* CreateArray(int size);
-int** CreateTwoDimArray(int columnsCount, int rowsCount);
 int* FillInArray(int* arr, int size);
-int** FillInTwoDimArray(int** arr, int columnsCount, int rowsCount);
 void ShowArray(int* arr, int size);
-void ShowTwoDimArray(int** arr, int columnsCount, int rowsCount);
