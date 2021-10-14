d0f08a588966f768494d9b4c3d7fa31bd4a46484
Ivan Orlov <orlovivan1907@outlook.com>
2021-10-14 15:35:53 +0300

    FavoritePoemApp is done

--- 2nd/C++/FavoritePoemApp/FavoritePoemApp/Source.cpp
+++ 2nd/C++/FavoritePoemApp/FavoritePoemApp/Source.cpp
@@ -3,5 +3,37 @@ using namespace std;
 
 void main()
 {
-	cout << "Hello";
+	cout << "To be, or not to be, that is the question"
+		"\nWhether 'tis nobler in the mind to suffer"
+		"\nThe slings and arrows of outrageous fortune,"
+		"\nOr to take arms against a sea of troubles"
+		"\nAnd by opposing end them.To die—to sleep,"
+		"\nNo more;and by a sleep to say we end"
+		"\nThe heart - ache and the thousand natural shocks"
+		"\nThat flesh is heir to : 'tis a consummation"
+		"\nDevoutly to be wish'd. To die, to sleep;"
+		"\nTo sleep, perchance to dream—ay, there's the rub:"
+		"\nFor in that sleep of death what dreams may come,"
+		"\nWhen we have shuffled off this mortal coil,"
+		"\nMust give us pause—there's the respect"
+		"\nThat makes calamity of so long life."
+		"\nFor who would bear the whips and scorns of time,"
+		"\nTh'oppressor's wrong, the proud man's contumely,"
+		"\nThe pangs of dispriz'd love, the law's delay,"
+		"\nThe insolence of office, and the spurns"
+		"\nThat patient merit of th'unworthy takes,"
+		"\nWhen he himself might his quietus make"
+		"\nWith a bare bodkin ? Who would fardels bear,"
+		"\nTo gruntand sweat under a weary life,"
+		"\nBut that the dread of something after death,"
+		"\nThe undiscovere'd country, from whose bourn"
+		"\nNo traveller returns, puzzles the will,"
+		"\nAnd makes us rather bear those ills we have"
+		"\nThan fly to others that we know not of ?"
+		"\nThus conscience doth make cowards of us all,"
+		"\nAnd thus the native hue of resolution"
+		"\nIs sicklied o'er with the pale cast of thought,"
+		"\nAnd enterprises of great pith and moment"
+		"\nWith this regard their currents turn awry"
+		"\nAnd lose the name of action.";
 }
